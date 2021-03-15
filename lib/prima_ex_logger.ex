06fc2db396c79bf34a33176cde30edf15905d1e1
gilacost <josep.dlacoste@prima.it>
2021-03-15 14:48:39 +0000

    Avoid using timex

--- lib/prima_ex_logger.ex
+++ lib/prima_ex_logger.ex
@@ -148,21 +148,22 @@ defmodule PrimaExLogger do
   def to_printable(v, _), do: inspect(v)
 
   @spec timestamp_to_iso(tuple()) :: String.t()
-  defp timestamp_to_iso({{year, month, day}, {hour, minute, second, milliseconds}}) do
+  def timestamp_to_iso({{year, month, day}, {hour, minute, second, milliseconds}}) do
     case NaiveDateTime.new(
-           year,
-           month,
-           day,
-           hour,
-           minute,
-           second,
-           milliseconds * 1000
+           %Date{
+             year: year,
+             month: month,
+             day: day
+           },
+           %Time{
+             hour: hour,
+             minute: minute,
+             second: second,
+             microsecond: {1000 * milliseconds, 6}
+           }
          ) do
       {:ok, ts} ->
-        ts
-        |> Timex.to_datetime()
-        |> Timezone.convert("Etc/UTC")
-        |> Timex.format!("{ISO:Extended}")
+        NaiveDateTime.to_iso8601(ts, :extended)
 
       _ ->
         nil
