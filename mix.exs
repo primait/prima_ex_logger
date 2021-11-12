4bcbd7d5ce46c048ff86d23fd0f238864a0be1e5
Michelangelo <michelangelo@morrillo.it>
2021-11-12 15:55:12 +0100

    bump version

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule PrimaExLogger.MixProject do
   def project do
     [
       app: :prima_ex_logger,
-      version: "0.2.2",
+      version: "0.2.3",
       source_url: "https://github.com/primait/prima_ex_logger",
       elixir: "~> 1.7",
       deps: deps(),
