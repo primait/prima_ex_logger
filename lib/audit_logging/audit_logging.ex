c1dbf6b5daca5de9fc161bd2a95a5ada058d1016
Cristiano Piemontese <cristiano.piemontese@prima.it>
2025-02-03 16:57:09 +0100

    red test

--- lib/audit_logging/audit_logging.ex
+++ lib/audit_logging/audit_logging.ex
@@ -1,5 +1,6 @@
 defmodule PrimaExLogger.AuditLogging do
   def log(audit_log) do
     IO.puts(Jason.encode!(audit_log)) 
+    :ok
   end
 end
--- test/audit_logging_test.exs
+++ test/audit_logging_test.exs
@@ -6,7 +6,7 @@ defmodule PrimaExLogger.AuditLoggingTest do
   alias PrimaExLogger.AuditLogging
   alias PrimaExLogger.AuditLogging.AuditLog
 
-  test "Happy case" do
+  test "MVP: only required fields" do
     io =
       capture_io(fn ->
         audit_log =
@@ -16,7 +16,28 @@ defmodule PrimaExLogger.AuditLoggingTest do
           |> AuditLog.message("message")
           |> AuditLog.timestamp("timestamp")
 
-        AuditLogging.log(audit_log)
+        :ok = AuditLogging.log(audit_log)
+      end)
+
+    log = Jason.decode!(io)
+
+    assert log["actor"] == "actor"
+    assert log["event_name"] == "event_name"
+    assert log["message"] == "message"
+    assert log["timestamp"] == "timestamp"
+  end
+
+  test "misssing required fields" do
+    io =
+      capture_io(fn ->
+        audit_log =
+          AuditLog.new()
+          |> AuditLog.actor("actor")
+          |> AuditLog.event_name("event_name")
+          |> AuditLog.message("message")
+
+        {:error, error} = AuditLogging.log(audit_log)
+        assert error == "Missing required field(s): timestamp"
       end)
 
     log = Jason.decode!(io)
